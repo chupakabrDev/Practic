49adaac9c47c9e5ad16e7da353b8ff14a5c874c1
chupakabrDev <@null>
2026-07-09 13:18:11 +0500

    добавлен модуль для записи в файл

---
+++ writer.c
@@ -0,0 +1,32 @@
+#include "writer.h"
+
+#include <stdlib.h>
+
+#include "util.h"
+
+Writer* createWriter(const char* filename) {
+    if (filename == nullptr) return nullptr;
+
+    Writer* writer = malloc(sizeof(Writer));
+    checkAllocateMem(writer);
+
+    FILE* file = fopen(filename, "wb");
+    checkOpenFile(file);
+
+    writer->file = file;
+
+    return writer;
+}
+
+size_t writeNext(const Writer *writer, const char *data, const size_t size) {
+    if (writer == nullptr || writer->file == nullptr) return 0;
+
+    return fwrite(data, sizeof(char), size, writer->file);
+}
+
+void destroyWriter(Writer *writer) {
+    if (writer == nullptr) return;
+
+    fclose(writer->file);
+    free(writer);
+}
---
+++ writer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+
+typedef struct Writer {
+    FILE* file;
+} Writer;
+
+Writer* createWriter(const char* filename);
+
+size_t writeNext(const Writer* writer, const char* data, size_t size); // вернет кол-во записанных байт
+
+void destroyWriter(Writer* writer);

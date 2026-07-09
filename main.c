cc063cddf9db94b176418ac39801a4745796bbd8
chupakabrDev <@null>
2026-07-09 13:23:20 +0500

    прототип работы, чтение

--- main.c
+++ main.c
@@ -1,3 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "finder.h"
+#include "reader.h"
+
+#define N 16
 
 int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Должно быть 3 аргумента\n");
+        return 1;
+    }
+
+    char* filename = argv[1];
+    char* original = argv[2];
+    char* replacement = argv[3];
+
+ptrdiff_t offset = (ptrdiff_t)strlen(original) - (ptrdiff_t)strlen(replacement);    printf("%td\n", offset);
+
+    Reader* reader = createReader(filename, N);
+    Finder* finder = createFinder(original, strlen(original)); // нулевой бат не берется потому что длина его не учитывает
+
+    size_t count = readNext(reader);
+    if (count == 0) {
+        puts("Пустой файл");
+        destroyReader(reader);
+        goto destroy;
+    }
+
+    while (count != 0) {
+
+        if (find(finder, reader->buffer, count)) {
+            Match* match = get(finder);
+            while (match != nullptr) {
+                printf("start: %lu; end: %lu\n", match->start, match->end);
+                match = get(finder);
+            }
+            free(match);
+        }
+
+        count = readNext(reader);
+    }
+
+    destroy: {
+        destroyReader(reader);
+        destroyFinder(finder);
+    }
+
+    return 0;
 }

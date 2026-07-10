d2af6c3fe381e3dd7917da576d4877e13f819afb
chupakabrDev <@null>
2026-07-10 13:38:52 +0500

    забыл добавить изменения

--- lib/tests/test_finder/test_finder.c
+++ lib/tests/test_finder/test_finder.c
@@ -1,3 +1,16 @@
-//
-// Created by aleksey on 10.07.2026.
-//
+#include <unity.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_lib_function(void) {
+    int result = some_function_from_lib(5);
+    TEST_ASSERT_EQUAL_INT(10, result);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_lib_function);
+    // добавьте другие тесты
+    return UNITY_END();
+}
